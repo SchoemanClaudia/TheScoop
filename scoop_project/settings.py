ea89e239c30edfa6e74b5b347fdfb4dd41eb1af5
Claudia Schoeman <claudiaschoeman1@gmail.com>
2025-01-05 18:00:17 +0000

    Create and add review app

--- scoop_project/settings.py
+++ scoop_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kru85x-svf%la+bjdgmm5uc@&0#elq*geq09nl_^f@-n_ejive
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-schoemanclaudi-thescoop-5whfsx86h9l.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reviews',
 ]
 
 MIDDLEWARE = [
--- scoop_project/urls.py
+++ scoop_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reviews.views import my_reviews
 
 urlpatterns = [
+    path('reviews/', my_reviews, name='reviews'),
     path('admin/', admin.site.urls),
 ]
