d315a51342d8de7840bda599b1274a19aa9dac71
Claudia Schoeman <claudiaschoeman1@gmail.com>
2025-01-11 20:30:57 +0000

    Install & setup Summernote

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import ScoopReview, ReviewComment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(ScoopReview)
+class ReviewsAdmin(SummernoteModelAdmin):
+    
+    list_display = ('location', 'slug', 'status', 'created_at')
+    search_fields = ['location', 'review']
+    list_filter = ('status', 'created_at')
+    prepopulated_fields = {'slug': ('location',)}
+    summernote_fields = ('review',)
+
 
 # Register your models here.
-admin.site.register(ScoopReview)
 admin.site.register(ReviewComment)
--- scoop_project/urls.py
+++ scoop_project/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# order of string paths alphabetical, with the empty string path last.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path("", include("reviews.urls"), name="reviews-urls"),
 ]
