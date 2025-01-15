f5f59b25d4504c2ee1d5b6797655401e1d0b4b64
Claudia Schoeman <claudiaschoeman1@gmail.com>
2025-01-15 21:45:54 +0000

    Add form to post_detail.html

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,8 +1,8 @@
-from .models import Comment
+from .models import ReviewComment
 from django import forms
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = Comment
-        fields = ('What is your opinion?',)
+        model = ReviewComment
+        fields = ('comment',)
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import ScoopReview
+from .models import ScoopReview, ReviewComment
 from .forms import CommentForm
 
 # Create your views here.
