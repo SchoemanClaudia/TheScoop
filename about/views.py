a58c25a8507d6cca8a07fbc8a4f890aba7ec16e3
Claudia Schoeman <claudiaschoeman1@gmail.com>
2025-01-16 11:53:07 +0000

    Create CollaborateForm view & variable

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 # Create your views here.
 
 
@@ -8,9 +9,13 @@ def meet_us(request):
     Renders the Meet Us page
     """
     about = About.objects.all().order_by('-updated_at').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/meet_us.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
+        },
     )
