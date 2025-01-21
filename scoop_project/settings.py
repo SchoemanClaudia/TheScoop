f689eb51727e25dc5ac8cc4ccc57b7453b871b34
Claudia Schoeman <claudiaschoeman1@gmail.com>
2025-01-21 20:37:29 +0000

    Add error msg templates

--- scoop_project/settings.py
+++ scoop_project/settings.py
@@ -28,7 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.path.isfile('env.py') else False
+# DEBUG = True if os.path.isfile('env.py') else False
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-schoemanclaudi-thescoop-5whfsx86h9l.ws.codeinstitute-ide.net",
