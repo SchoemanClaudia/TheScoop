b9f760f86ee2ffecea923a12b9882888b42a99aa
Claudia Schoeman <claudiaschoeman1@gmail.com>
2025-01-22 21:58:45 +0000

    Add & run test files

--- scoop_project/settings.py
+++ scoop_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True if os.path.isfile('env.py') else False
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-schoemanclaudi-thescoop-5whfsx86h9l.ws.codeinstitute-ide.net",
@@ -112,6 +113,9 @@ WSGI_APPLICATION = 'scoop_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
